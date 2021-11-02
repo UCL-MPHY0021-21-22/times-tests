9fc3b8ceb7b3a2a1435a15f25b200b583716ab22
Susizhou <susanachen2000@gmail.com>
2021-11-02 17:38:11 +0000

    result and expected variable addition

--- test_times.py
+++ test_times.py
@@ -8,3 +8,7 @@ def test_given_input():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
 
+        result = ... 
+    expected = ...
+    assert result == expected
+
