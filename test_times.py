c896f415d2ad9fd7abc2135882138c763ce76999
Susizhou <susanachen2000@gmail.com>
2021-11-02 18:59:11 +0000

    added no overlap test and code fixed

--- test_times.py
+++ test_times.py
@@ -8,7 +8,17 @@ def test_given_input():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
 
     result = compute_overlap_time(large, short) 
-    h = time_range("2010-01-14 10:30:00", "2010-01-15 10:45:00")
-    expected = compute_overlap_time(short, h)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    
     assert result == expected
+
+def test_no_overlap():
+    test1 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    test2 = time_range("2010-01-12 13:00:00", "2010-01-12 15:00:00")
+
+    result1 = compute_overlap_time(test1, test2) 
+    expected = []
+
+    assert result1 == expected
+
 
