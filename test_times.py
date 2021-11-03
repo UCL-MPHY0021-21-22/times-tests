7c2a4fdcc20ffbdd7efe1ddcec1b8d8101df783c
matthewtam17 <matthewcatsey@gmail.com>
2021-11-03 18:04:15 +0000

    Added a unit test file, and a single unit test for compute_overlap_time

---
+++ test_times.py
@@ -0,0 +1,8 @@
+from times import *
+
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large, short)
+    expected = compute_overlap_time(large, short)
+    assert result == expected
