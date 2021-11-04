e643535b7dd8af126b7dd5da63d1438a010074d1
Pete Jones <peterjones252@gmail.com>
2021-11-04 14:56:17 +0000

    Paramtrize tests

--- test_times.py
+++ test_times.py
@@ -1,33 +1,20 @@
 from times import *
+import pytest
 from pytest import raises
 
-def test_given_input():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large, short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    assert result == expected
+test_range_0 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+test_range_1 = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+test_range_2 = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60)
+test_range_3 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60)
+test_range_4 = time_range("2010-01-12 12:00:00", "2010-01-12 12:45:00")
 
-def test_no_overlap():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60)
-    result = compute_overlap_time(large, short)
-    expected = []
-    assert result == expected
+expected_0 = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+expected_1 = []
 
-def test_several_intervals():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60)
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large, short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    assert result == expected
-
-def test_same_time():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 12:00:00", "2010-01-12 12:45:00")
-    result = compute_overlap_time(large, short)
-    expected = []
-    assert result == expected
+@pytest.mark.parametrize("time_range_1,time_range_2,expected", [(test_range_0,test_range_1, expected_0), (test_range_0, test_range_2, expected_1), \
+    (test_range_3, test_range_1, expected_0), (test_range_0, test_range_4, expected_1)])
+def test_overlap(time_range_1, time_range_2, expected):
+    assert compute_overlap_time(time_range_1, time_range_2) == expected
 
 def test_negative():
     with raises(ValueError, match ="Start time must be before end time."):
