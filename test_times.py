ce61872837401a75ecdbb4aa628920a5db567ae2
CoreyFulop <corey.fulop@gmail.com>
2021-11-04 11:37:31 +0000

    Some more tests!

--- test_times.py
+++ test_times.py
@@ -10,3 +10,50 @@ def test_given_input():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     
     assert result == expected   
+    
+    
+from times import time_range, compute_overlap_time
+
+def test_no_overlap_years():
+    
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2012-01-12 10:30:00", "2012-01-12 10:45:00", 2, 60)
+    
+    result = compute_overlap_time(large, short)
+    
+    expected = []
+    
+    assert result == expected   
+
+def test_no_overlap_switched():
+    
+    large = time_range("2012-01-12 10:00:00", "2012-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    
+    result = compute_overlap_time(large, short)
+    
+    expected = []
+    
+    assert result == expected   
+    
+def test_several_intervals():
+    
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 4)
+    short = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00", 2)
+    
+    result = compute_overlap_time(large, short)
+    
+    expected = [('2010-01-12 11:00:00', '2010-01-12 11:30:00'), ('2010-01-12 11:30:00', '2010-01-12 12:00:00')]
+    
+    assert result == expected   
+    
+def similtaneous():
+    
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 13:45:00", 2, 60)
+    
+    result = compute_overlap_time(large, short)
+
+    expected = [('2010-01-12 12:00:00', '2010-01-12 12:00:00')]
+    
+    assert result == expected
