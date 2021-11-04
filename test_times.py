17c1663d2142064310c739c3d07b5838938ff638
Yahya Scerbo <scerbo.robert@gmail.com>
2021-11-04 12:40:27 +0000

    Answers Adding a Continuous Integration system #19

--- test_times.py
+++ test_times.py
@@ -1,4 +1,6 @@
+from typing import Match
 from times import compute_overlap_time,time_range
+import pytest
 
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -6,3 +8,15 @@ def test_given_input():
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_backwards_range():
+    with pytest.raises(ValueError,Match='m'):
+        time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
+
+def test_dont_overlap():
+    large2 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short2 = time_range("2011-01-12 10:00:00", "2011-01-12 12:00:00")
+    result = compute_overlap_time(large2,short2)
+    expected = []
+    print(result)
+    assert (result == expected),'Overlap does not exist'
