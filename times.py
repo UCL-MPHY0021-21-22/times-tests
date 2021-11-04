4d4c8f2fa6d287db27ef5ae8c09e9a073edffe92
Thomas <thomasclarke28@hotmail.co.uk>
2021-11-04 12:01:59 +0000

    Group work up to break 2 (negative testing)

--- times.py
+++ times.py
@@ -4,6 +4,10 @@ import datetime
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    #print(type(start_time_s)
+    if start_time_s > end_time_s:
+        raise ValueError("Invalid time range, end time is before start time")
+
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
@@ -18,6 +22,7 @@ def compute_overlap_time(range1, range2):
             low = max(start1, start2)
             high = min(end1, end2)
             overlap_time.append((low, high))
+
     return overlap_time
 
 
